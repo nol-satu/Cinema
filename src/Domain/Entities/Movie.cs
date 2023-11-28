033729ff115887f6c375c5fcf63716572401c3ca
Immanuel <ntxvir@gmail.com>
2023-11-28 11:30:08 +0700

    Add project files.

---
+++ src/Domain/Entities/Movie.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities;
+
+public record Movie
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public float Rating { get; set; }
+    public string Synopsis { get; set; } = "";
+    public decimal TicketPrice { get; set; }
+}
